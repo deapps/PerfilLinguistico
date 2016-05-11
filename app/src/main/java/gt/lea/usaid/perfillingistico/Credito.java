13ead0870902bc0b4ca04be4029b552264402538
MinZoo <intecapkomgit6@gmail.com>
2016-05-11 16:33:56 -0600

    Initial commit

---
+++ app/src/main/java/gt/lea/usaid/perfillingistico/Credito.java
@@ -0,0 +1,17 @@
+package gt.lea.usaid.perfillingistico;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Credito extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_credito, container, false);
+    }
+
+}
