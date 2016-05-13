31308046f9c35e1c7fdb030db899953bc394015a
app <desarrolladoresdeapps@gmail.com>
2016-05-13 04:51:01 -0600

    comitt1

--- app/src/main/java/gt/lea/usaid/perfillingistico/Serie.java
+++ app/src/main/java/gt/lea/usaid/perfillingistico/Serie.java
@@ -1,29 +1,27 @@
 package gt.lea.usaid.perfillingistico;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class Serie extends Fragment {
-
-
-    public Serie() {
-        // Required empty public constructor
-    }
+    Button b1;
+    Intent intent;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_serie, container, false);
     }
+
 
 }
